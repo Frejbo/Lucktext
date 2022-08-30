50afcacbe435256691e18e2ae4e7b4bdf4a4ce87
Frejbo <frej.hellsten@gmail.com>
2022-08-30 10:32:20 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿string[] questions = {"Vad heter du?", "Hur gammal är du?", "Hur lång är du?", "Vad heter din hamster?", "Har du en hamster?"};
+string[] answers = {};//"", "", "", "", ""};
+
+int index = 0;
+foreach (string ask in questions) {
+    Console.WriteLine(ask);
+    // answers[index] = ;
+    answers.Append(Console.ReadLine());
+    index = index++;
+}
+
+Console.WriteLine("Uploading personal data to tracker.facebook.com");
+
+for (string i = "."; i != "..."; i = string.Join() oin(".")) {
+    System.Threading.Thread.Sleep(500);
+    Console.WriteLine()
+}
+
+Console.ReadLine();
+
+// for (int i = 0; i < 3; i++) {
+//     Console.ReadLine;
+// }
+
+// Console.WriteLine("Vad heter du?");
+// string name = Console.ReadLine();
+
+// Console.WriteLine($"Hej {name}");
+
+// int count = 0;
+
+// Console.ReadLine();
+// // while (true) {
+// //     Console.WriteLine(name);
+// // }
