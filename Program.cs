803f003552aa57c5aa712020cafdaad3de735952
Frejbo <frej.hellsten@gmail.com>
2022-08-30 11:04:18 +0200

    Principen är klar men några buggar som ska fixas

--- Program.cs
+++ Program.cs
@@ -1,20 +1,32 @@
 ﻿string[] questions = {"Vad heter du?", "Hur gammal är du?", "Hur lång är du?", "Vad heter din hamster?", "Har du en hamster?"};
-string[] answers = {};//"", "", "", "", ""};
+// string[] answers = new string[4]; //"", "", "", "", ""};
+
+List<string> answers = new List<string>();
 
 int index = 0;
 foreach (string ask in questions) {
     Console.WriteLine(ask);
     // answers[index] = ;
+    // if (ask == questions[2] && )
     answers.Append(Console.ReadLine());
     index = index++;
 }
 
 Console.WriteLine("Uploading personal data to tracker.facebook.com");
 
-for (string i = "."; i != "..."; i = string.Join() oin(".")) {
-    System.Threading.Thread.Sleep(500);
-    Console.WriteLine()
+for (string i = "."; i != "....";  i = i+".") {
+    System.Threading.Thread.Sleep(400);
+    // Console.WriteLine(string.Join(i, "."));
+    Console.WriteLine(i);
+    System.Threading.Thread.Sleep(250);
 }
+Console.WriteLine(answers[4].Contains("ej"));
+if (answers[4].Contains("ej")) {
+    Console.WriteLine($"Hej {answers[0]}, har du verkligen ingen hamster som heter {answers[3]}? Känns olikt dig, fast i och för sig... du är ju faktiskt lite som en hamster själv, med ditt {answers[1]} års gamla skinn. Du skulle säkert ändå trampa ihjäl den eftersom du är så mycket längre än den. du är ju {answers[2]} medan den bara är ca 15 cm...");
+} else {
+    Console.WriteLine($"Hej {answers[0]}, har du verkligen en hamster som heter {answers[3]}? Känns inte så rimligt... det är liksom ett otroligt konstigt namn på en hamster... inte sant? Men jag antar att det är sån du är, hade inte förväntat mig så mycket mer om jag ska vara ärlig... du är ju faktiskt lite som en hamster själv. Visste du att {answers[1]} åringar som är {answers[2]} långa tenderar att vara mycket sämra på att döpa just hamstrar? Eller nej förstås... sånt vet ju inte du...");
+}
+
 
 Console.ReadLine();
 
