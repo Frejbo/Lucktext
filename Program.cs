b3bd07f3ab0d49f22d1744d596f2b673d51ead43
Frejbo <frej.hellsten@gmail.com>
2022-09-01 16:15:53 +0200

    Snyggade till lite

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿string[] questions = {"Vad heter du?", "Hur gammal är du?", "Hur lång är du?", "Vad heter din hamster?", "Har du en hamster?"};
 
-List<string> answers = new List<string>();// Lyckas verkligen inte läsa data ur denna, förväntade mig något som answers[i] men gick ej. 
+List<string> answers = new List<string>();
 
 foreach (string ask in questions) {
     Console.WriteLine(ask);
@@ -12,15 +12,14 @@ Console.WriteLine("Uploading personal data to tracker.facebook.com");
 
 for (string i = "."; i != "....";  i = i+".") {
     System.Threading.Thread.Sleep(400);
-    // Console.WriteLine(string.Join(i, "."));
     Console.WriteLine(i);
     System.Threading.Thread.Sleep(250);
 }
+
 if (answers[4].ToLower().Contains("ej")) {
     Console.WriteLine($"Hej {answers[0]}, har du verkligen ingen hamster som heter {answers[3]}? Känns olikt dig, fast i och för sig... du är ju faktiskt lite som en hamster själv, med ditt {answers[1]} års gamla skinn. Du skulle säkert ändå trampa ihjäl den eftersom du är så mycket längre än den. du är ju {answers[2]} medan den bara är ca 15 cm...");
 } else {
     Console.WriteLine($"Hej {answers[0]}, har du verkligen en hamster som heter {answers[3]}? Känns inte så rimligt... det är liksom ett otroligt konstigt namn på en hamster... inte sant? Men jag antar att det är sån du är, hade inte förväntat mig så mycket mer om jag ska vara ärlig... du är ju faktiskt lite som en hamster själv. Visste du att {answers[1]} åringar som är {answers[2]} långa tenderar att vara mycket sämre på att döpa just hamstrar? Eller nej förstås... sånt vet ju inte du...");
 }
-
 
 Console.ReadLine(); //Behåller terminalen öppen tills något trycks.
